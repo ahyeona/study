6ff84967476450cf9b43a3bf528de99ff0dcf658
ahyeona <ahhyeon741@gmail.com>
2023-09-12 14:43:13 +0900

    0912

--- 20230912/05.wallet/src/core/__test/block.test.ts
+++ 20230912/05.wallet/src/core/__test/block.test.ts
@@ -82,7 +82,7 @@ describe("block 검증", ()=>{
         // 현재 블록을 생성한다 가정하고
         // 현재 블록이 생성된 시간이 이전 10번째 블록으로부터 얼마나 걸렸는지
         // 확인하고 블록의 정해진 생성 주기보다 빠르면 난이도 올리고 아니면 내린다.
-        for (let i = 0; i < 50; i++) {
+        for (let i = 0; i < 1; i++) {
             let block = Block.generateBlock(newChain.latestBlock(), ["block"], newChain.getAdjustmentBlock());
             newChain.addToChain(block);
         }
@@ -90,3 +90,177 @@ describe("block 검증", ()=>{
         // console.log("newChain",newChain);
     });
 })
+
+// 지갑 구성
+// 개인키, 공개키, 서명
+// 지갑 주소 / 계정 만들기
+
+// 개인키와 공개키와 서명을 이용한 신원 인증 방식은 분산 원장이라는 이해가 필요
+
+// 분산원장 : 장부를 모두가 관리하는 것. 데이터의 합의 기술
+
+// crypto, crypto-js, elliptic (타원 곡선 알고리즘 라이브러리)
+
+// npm i -D crypto @types/crypto
+// npm i -D crypto-js @types/crypto-js
+// npm i -D elliptic @types/elliptic
+
+import { randomBytes } from "crypto"
+import elliptic from "elliptic"
+import { SHA256 } from "crypto-js";
+
+const ec = new elliptic.ec("secp256k1");
+
+// secp256k1은 비트코인과 이더리움에서 사용되는 알고리즘
+// 키 생성 및 디지털 서명(검증하기 위해서. 영수증), 주소 생성
+// 타원 곡선의 별명
+
+// 전달하는 사람과 받는 사람 등 모든 사람들은 공통적으로 타원 곡선의 한 점을 알고 있어야 하는데
+// 이 점을 타원곡선 기준점 G라고 부름
+
+// 타원 곡선의 기준점 좌표가 뭐냐에 따라 타원 곡선 별명을 붙여준다.
+
+// 비트코인과 이더리움에서 사용되는 타원곡선 별명은 secp256k1임
+
+// y^2 = x^3 + ax + b
+// 이 방정식에 만족하는 곡선
+
+// a가 0, b가 7
+// 기준점 G는 x 및 y 좌표를 16진수로 표현한 것
+// 02 79BE667E F9DCBBAC 55A06295 CE870B07 029BFCDB 2DCE28D9 59F2815B 16F81798
+
+// y^2 = x^3 + 7
+
+// A가 트랜잭션 만들고 서명을 만들고 (영수증)
+// 본인들 볼펜이 하나씩 있어서 (개인키)
+// 볼펜 준비. 타원 곡선의 지정 범위 내의 값으로 정한다. (1 ~ n-1 까지의 범위) (범위내의 정수)
+// secp256k1의 n은 1.157920892373162e+77값 (과학 표기법)
+// 16진수로 변환하면 FFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFEBAAEDCE6AF48A03BBFD25E8CD0364141
+// FFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFEBAAEDCE6AF48A03BBFD25E8CD0364141에서 -1
+// 즉 개인키를 1 ~ FFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFEBAAEDCE6AF48A03BBFD25E8CD0364140 까지의 숫자 범위 중에 하나를 사용하는 것
+
+// 개인키를 임시로 하나 지정해보면
+// FFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFEBAAEDCE6AF48A03BBFD25E8CD0364140
+
+// 전자 서명을 만들 때 2개의 서명이 필요하다.
+
+// 서명 r : 트랜잭션을 보낼 때 개인키처럼 정수를 뽑음. 이 값을 k라고 함. 서명 r = k * G(기준점)
+
+// 서명 s : 공식 == k⁻¹ = (z + r * private key) mod n    k를 역수 계산 z + r * 개인키 나머지 n
+// k = 서명 r을 만들 때 뽑은 랜덤값
+// z = 만든 트랜잭션 정보
+// r = 서명 r
+// 개인키 = 범위에서 지정하고 본인만 알고 있는 개인키
+// mod n = n으로 나눈 나머지
+
+// 중요한 건 서명 s를 만드는데 개인키를 사용했다는 개념
+
+// w 동일한 서명을 만들지 않기 위해서 추가한 임의의 값
+// w = s⁻¹ mod n
+// U1 = z * w mod n
+// U2 = r * w mod n
+// U1 * G + U2 * 공개키   값을 구해서 서명 r과 같은지 비교해서 검증
+
+// 이걸 전부 이해할 필요는 없고 중요한 중심만 이해하면 됨
+// 개인키로 서명을 만든 거고
+// 공개키를 통해서 서명을 검증할 수 있다.
+
+// 데이터 전송
+// 1. 트랜잭션 생성
+// 2. 개인키 생성
+// 3. 서명 r, 서명 s 생성
+
+// 데이터 수신
+// 1. U1 * G + U2 * 공개키 로 값을 구해서 서명 r과 비교(검증)
+
+describe("지갑 만들기", () => {
+    let privKey : string;
+    let pubKey : string;
+    let signature : elliptic.ec.Signature;
+
+    it("개인키 생성", ()=>{
+        // 2진수의 랜덤값을 만들자
+        // 16진수로 나타냄
+        privKey = randomBytes(32).toString("hex");
+        console.log("개인키 : ", privKey);
+        // 개인키의 길이 64
+        console.log("개인키 길이 : ", privKey.length);
+    });
+
+    it("공개키 생성", ()=>{
+        const keyPair = ec.keyFromPrivate(privKey);
+        // encode 두번째 매개변수 false : 문자열 압축 여부. 중요하지 않음
+        // 개인키로 공개키를 생성
+        pubKey = keyPair.getPublic().encode("hex", false);
+        console.log("공개키 : ", pubKey);
+        // 공개키는 130자의 문자열
+        console.log("공개키 길이 : ", pubKey.length);
+    });
+
+    it("서명 만들기", ()=>{
+        const keyPair = ec.keyFromPrivate(privKey);
+        // 임시 트랜잭션 내용
+        const hash = SHA256("transaction data").toString();
+        // sign : 서명 생성 메서드
+        signature = keyPair.sign(hash, "hex");
+        console.log("서명 : ", signature);
+
+        // r 서명, s 서명
+        // BN = BigNumber 무척 큰 number 타입
+        // negative 0 : 양수
+        // words : r서명이나 s서명의 값을 32비트 정수 배열로 표시한 값
+        // length : 배열의 길이
+        // red : 중복..? 무시하셔도 됨
+
+        // Signature {
+        //     r: BN {
+        //       negative: 0,
+        //       words: [
+        //         32078582, 26792923,
+        //         53371064,  5604287,
+        //          5903284, 16963250,
+        //         37811899, 49672370,
+        //         12595635,   708191
+        //       ],
+        //       length: 10,
+        //       red: null
+        //     },
+        //     s: BN {
+        //       negative: 0,
+        //       words: [
+        //         36538900, 63481584, 45463146, 33317876,
+        //         13748056, 53780964,  8696998, 23268939,
+        //         10375762,  4016054,        0,        0,
+        //                0,        0,        0,        0,
+        //                0,        0,        0,        0,
+        //                0,        0,        0,        0,
+        //                0,        0,        0,        0,
+        //                0,        0
+        //       ],
+        //       length: 10,
+        //       red: null
+        //     },
+        //     recoveryParam: 0
+        //   }
+    });
+
+    it("검증하기", ()=>{
+        const hash = SHA256("transaction data").toString();
+        const verify = ec.verify(hash, signature, ec.keyFromPublic(pubKey, "hex"));
+        console.log("검증됨? : ",verify);
+    });
+
+    // 지갑 주소 생성
+    it("지갑 주소", ()=>{
+        // 계정 만드는 방법은 만든 공개키의 값에서 26개의 문자열을 앞에서 잘라서
+        // 40자리만큼을 남겨서 주소로 사용한다.
+        // 불필요한 부분 제거하고 앞에 가독성을 위해 0x 붙이기
+        // 주소의 앞에는 0x 붙이는 것이 일반적 (지갑주소 16진수라는 뜻)
+        const address = pubKey.slice(26).toString();
+        console.log("주소 : ", `0x${address}`);
+    });
+
+});
+
+
+
